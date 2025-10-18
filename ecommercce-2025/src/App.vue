d4baf3cec1137c3ad949805faf8aeb0d5fc702e9
Neth Tithyarita <neth.tithyarita12345@gmail.com>
2025-10-18 10:54:55 +0700

    new-commit

---
+++ ecommercce-2025/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    This is my first VueJs project
+  </div>
+</template>
+
+<script setup lang="ts">
+// You don't need to import anything for now
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 100px;
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
